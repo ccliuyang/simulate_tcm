c7f988a92e658439f06743e3c088a4ff43f1c651
liuyang <416954509@qq.com>
2020-04-21 14:35:44 +0800

    首次提交

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from enum import Enum
+
+class Identifier(object):
+    def __init__(self, **kwargs):
+        self.__dict__ = kwargs
+
+
+
+if __name__ == "__main__":
+    pass
+    
